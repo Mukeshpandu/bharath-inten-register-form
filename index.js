469ad11b4f1215465043ed6cc59f44793717307b
Mukesh Basappagari <141038139+Mukeshpandu@users.noreply.github.com>
2023-11-21 18:42:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,18 @@ app.get("/signup",(req,res)=>{
     res.sendFile(__dirname + "/public/signup.html")
 })
 
+
+
+
 app.post("/signup", (req,res)=>{
 
-const user= new User({
-    name:req.body.name,
-    password:req.body.password
+    const user= new User({
+        name:req.body.name,
+        password:req.body.password
 });
+
+
+
 
 User.find({}).then((users) => {
     const u = users;
@@ -43,9 +49,9 @@ User.find({}).then((users) => {
     }
     
 })
+   
 
 
-    
 })
 
 app.post("/login", (req, res) => {
@@ -54,7 +60,28 @@ app.post("/login", (req, res) => {
         password:req.body.password
     });
 
+
+
+User.find({}).then((users) => {
+    const u = users;
+    
+    const x = users.find(y => y.name === user.name)
+
+    if (x) {
+        console.log("User already exists")
+        return res.sendFile(__dirname + "/public/error.html")
+    } else {
+        user.save().then(savedUser => {
+            console.log(savedUser);
+          })
+        return res.sendFile(__dirname + "/public/success.html")
+
+    }
+    
 })
+})
+   
+
 
 app.listen(3000,()=>{
     console.log("port connected");
